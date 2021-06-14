62d8cfa12db0df926a7f9721ee845f7060336371
MaciejP <m.ponicki1995@gmail.com>
2021-06-14 13:38:14 +0200

    add index.js and styles.js to Content component add styles in Content component remove border in index.css

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import NavBar from "./components/NavBar";
 import Heading from "./components/Heading";
+import Content from "./components/Content";
+
 
 ReactDOM.render(
   <React.StrictMode>
     <div className="wrapper">
       <NavBar />
       <Heading />
+      <Content />
     </div>
   </React.StrictMode>,
   document.getElementById("root")
