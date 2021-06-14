ac47f2e9d4ad2fe6dbd9f8b13a9c3c1463720b4d
MaciejP <m.ponicki1995@gmail.com>
2021-06-14 23:32:58 +0200

    remove some icons specify in manifest.json add some styles on Footer component add function that change the year in footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,5 +1,27 @@
+import { useState } from "react";
+import { MainWrapper } from "./styles";
+
 const Footer = () => {
-  return <div>2</div>;
+  var [date, setDate] = useState();
+
+  const DateOfYear = (yr) => {
+    var today = new Date();
+    return (yr = today.getFullYear(setDate));
+  };
+
+  return (
+    <MainWrapper>
+      <p>“All about plants” {DateOfYear(date)}</p>
+      <p>
+        <span>
+          <strong>Any questions?</strong>
+        </span>
+        <span>
+          Contact us on <a href="mailto:example@plant.com">contact@plant.com</a>
+        </span>
+      </p>
+    </MainWrapper>
+  );
 };
 
 export default Footer;
